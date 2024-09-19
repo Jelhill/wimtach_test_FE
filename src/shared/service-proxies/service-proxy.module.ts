1d2d8c85adbccc3f878a10e68ed3791a7210615a
Jelili Umaru <talk2jelili2000@gmail.com>
2024-09-19 13:13:42 -0400

    Added refresh.sh file and proxies files.

--- src/shared/service-proxies/service-proxy.module.ts
+++ src/shared/service-proxies/service-proxy.module.ts
@@ -13,7 +13,8 @@ import * as ApiServiceProxies from './service-proxies';
         ApiServiceProxies.TokenAuthServiceProxy,
         ApiServiceProxies.AccountServiceProxy,
         ApiServiceProxies.ConfigurationServiceProxy,
-        { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true }
+        { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true },
+        ApiServiceProxies.StudentServiceProxy
     ]
 })
 export class ServiceProxyModule { }
